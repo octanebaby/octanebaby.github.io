c1adede45967f2a287bcefee20e6b36d8613f37a
Janine Yoong <janineyoong@Janines-Air.home>
2017-03-01 20:37:48 -0500

    added family

---
+++ Mapillary_family.js
@@ -0,0 +1,5 @@
+var project_title = 'Mapillary family';
+var project_subtitle = 'Unwittingly caught in the frame';
+var images = ['C9T46-i2x_hRURXIVSYlqg','spyIiL50EEy3lhy9VGRkIw','I8ozI2XlV6NXyGhKvIkHJw'];
+var titles = ['Otis in NYC','Tom in Sweden','Meha in Nikoi Island'];
+var content = ['Just trying a new iOS build in the park','Just testing the LG360 on a stroller walk','Just giving the Theta S a whirl on a beach vacation'];
